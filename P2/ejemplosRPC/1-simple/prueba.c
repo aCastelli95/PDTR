54dc4d9c42523d65da5fe43e4bc483c9733bcd29
aCastelli95 <castelli_alanunlp@hotmail.com>
2017-09-29 15:15:59 -0300

    Re-organizado para entregar

--- P2/ejemplosRPC/1-simple/prueba.c
+++
@@ -1,9 +0,0 @@
-Ejmpleo de texto
-
-	COntiene información que bueno, es informativa.
-Pepeeeee...rompe pepe rompeee. Te conte la historia del mago sin dientes?
-Sabe que la vi ayer.
-
-ALOOOOOO
-FIrma alalalalalallaannnn
-
