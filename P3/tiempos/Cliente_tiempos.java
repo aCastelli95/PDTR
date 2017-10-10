f5a168b3e83837c77becce255d5894cc9c290b78
Alan Fabian Castelli <castelli_alanunlp@hotmail.com>
2017-10-09 23:25:59 -0300

    Tiempo calculaod, falta ver como hacer un timeout() concurrente o con u if, preguntar

--- P3/tiempos/Cliente_tiempos.java
+++ P3/tiempos/Cliente_tiempos.java
@@ -5,13 +5,21 @@ public class Cliente_tiempos
   public static void main(String[] args)
   {
     // declarar variables
-   
+    double inicio_tiempo, fin_tiempo;
+    //inicio del tiempo, antes que de realiza la comunicacion con la interfaz.
+    inicio_tiempo = System.currentTimeMillis()/1000.0;
+    System.out.println("Inicio = "+ (inicio_tiempo));
     try
     {
       Interfaz_tiempos mir = (Interfaz_tiempos)java.rmi.Naming.lookup("//" + 
                               args[0] + ":" + args[1] + "/PruebaRMI");
+
+    mir.prueba_tiempos(); // realiza una llamada al servidor.
+    fin_tiempo = System.currentTimeMillis()/1000.0;
+    //resultado = fin_tiempo - inicio_tiempo;
+    System.out.println("Finalizacion = "+fin_tiempo);
+    System.out.println("Tiempo final de ejecucion =" +(fin_tiempo - inicio_tiempo) );
       
-      // aca el codigo
     } catch (Exception e)
       {
         e.printStackTrace();
--- P3/tiempos/Servidor_tiempos.java
+++ P3/tiempos/Servidor_tiempos.java
@@ -11,8 +11,16 @@ implements Interfaz_tiempos
   }
 
   public int prueba_tiempos() throws java.rmi.RemoteException
-  {
-
+  { int tiempo = 20000;
+     try{
+        if(tiempo > 10000){
+            Thread.sleep(tiempo);
+        }else{
+            Thread.sleep(tiempo);
+        }
+     }catch(InterruptedException e){}
+     System.out.println("Transcurrieron = "+ tiempo);
+     
     return 1;
 
   }
