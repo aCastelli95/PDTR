f8a37ad1bd7d83a863909db198b7db5028306606
Alan Castelli <castelli_alanunlp@hotmail.com>
2017-10-15 10:32:57 -0300

    Listo el cliente y servidor de tiempos

--- P3/tiempos/Cliente_tiempos.java
+++ P3/tiempos/Cliente_tiempos.java
@@ -5,6 +5,7 @@ public class Cliente_tiempos
   public static void main(String[] args)
   {
     // declarar variables
+    int total = 0;
     double inicio_tiempo, fin_tiempo;
     //inicio del tiempo, antes que de realiza la comunicacion con la interfaz.
     inicio_tiempo = System.currentTimeMillis()/1000.0;
@@ -13,16 +14,21 @@ public class Cliente_tiempos
     {
       Interfaz_tiempos mir = (Interfaz_tiempos)java.rmi.Naming.lookup("//" + 
                               args[0] + ":" + args[1] + "/PruebaRMI");
-
-    mir.prueba_tiempos(); // realiza una llamada al servidor.
+       
+    //System.out.println(mir.get_mensaje());	
+    total = mir.prueba_tiempos(); // realiza una llamada al servidor.
     fin_tiempo = System.currentTimeMillis()/1000.0;
     //resultado = fin_tiempo - inicio_tiempo;
-    System.out.println("Finalizacion = "+fin_tiempo);
+    System.out.println("Finalizacion = " + fin_tiempo);
     System.out.println("Tiempo final de ejecucion =" +(fin_tiempo - inicio_tiempo) );
-      
-    } catch (Exception e)
-      {
-        e.printStackTrace();
+    if(total == 1 ){
+        System.out.println("Timeout()");
       }
+    }
+	catch (Exception e)
+      {
+    e.getStackTrace();
+       }
+ 
  }
 }
--- P3/tiempos/Servidor_tiempos.java
+++ P3/tiempos/Servidor_tiempos.java
@@ -4,24 +4,39 @@ public class Servidor_tiempos
 extends java.rmi.server.UnicastRemoteObject 
 implements Interfaz_tiempos
 {
-  
+  int cantidad_entradas = 0;
   public Servidor_tiempos() throws java.rmi.RemoteException
   {
       // nada
   }
 
   public int prueba_tiempos() throws java.rmi.RemoteException
-  { int tiempo = 20000;
-     try{
-        if(tiempo > 10000){
-            Thread.sleep(tiempo);
-        }else{
-            Thread.sleep(tiempo);
-        }
-     }catch(InterruptedException e){}
-     System.out.println("Transcurrieron = "+ tiempo);
-     
-    return 1;
+  {
+	  
+    int tiempo = 31000; boolean seguir = true;
+    try{
+       //if(tiempo > 10000){
+        //   Thread.sleep(tiempo);
+       //}else{
+         //  Thread.sleep(tiempo);
+       //}
+       
+       //Thread.sleep(tiempo);
+       
+       if(cantidad_entradas == 1){
+         return 1;
+       }else{
+        cantidad_entradas++;
+        Thread.sleep(tiempo);
+        
+       }
+       
+  }
+ catch(InterruptedException e){
+       System.out.println("No Transcurrieron = 310 seg.");
+ }
+ System.out.println("Transcurrieron = 310 seg.");
+   return 2;
 
   }
 
@@ -33,7 +48,8 @@ implements Interfaz_tiempos
       Interfaz_tiempos mir = new Servidor_tiempos();
       java.rmi.Naming.rebind("//" + java.net.InetAddress.getLocalHost().getHostAddress() +
                               ":" + args[0] + "/PruebaRMI", mir);
-    }
+    
+                            }
     catch (Exception e)
     {
     }
